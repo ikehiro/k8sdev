d01bad0491f7ba0523920fefcbaa0c9dec718f8c
Hirotaka Ikeda <Hirotaka_Ikeda@lab-imac.local>
2020-07-08 15:17:59 +0900

    add new files. boot app passed.

---
+++ multi-project/customer-context/src/main/java/com/msa/customer/AccessingDataMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.msa.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.msa.customer.AccessingDataMysqlApplication.class, args);
+	}
+}
