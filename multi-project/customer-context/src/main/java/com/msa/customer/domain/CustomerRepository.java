b0ef1fbf5f6b4eed49bb36ea1530563f17428355
Hirotaka Ikeda <Hirotaka_Ikeda@lab-imac.local>
2020-08-05 15:59:06 +0900

    ドメインリポジトリに欲しい処理を定義

--- multi-project/customer-context/src/main/java/com/msa/customer/domain/CustomerRepository.java
+++ multi-project/customer-context/src/main/java/com/msa/customer/domain/CustomerRepository.java
@@ -1,4 +1,19 @@
 package com.msa.customer.domain;
 
 public interface CustomerRepository {
+
+    /**
+     * 顧客情報を永続化する
+     *
+     * @param customer
+     */
+    void save(Customer customer);
+
+    /**
+     * 顧客情報を取得する
+     *
+     * @param customerId 顧客ID
+     * @return 顧客情報
+     */
+    Customer findById(Long customerId);
 }
